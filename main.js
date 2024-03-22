2c47c1d8df3f8aae248cbeb5c5e1fa998b516727
abbos-khamidov <khamidov3538@gmail.com>
2024-03-22 20:12:42 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// Переменные
+
+// let a = 20;
+// a = 30;
+// console.log(a + 20)
+
+// alert
+// prompt
+// confirm
+
+// alert('Samina think!')
+// confirm('Are u sure?')
+
+// let age = +prompt('How old are u?');
+// // console.log(age)
+// console.log(typeof(age))
+
+// ConvertToInt32
+// js - data types
+// integer = int - целое число
+// string = str = текст
+// boolean  =True False
+
+
+
+// Условия - conditions
+// if else statemetss
+
+// if (условие) {
+//     действие
+// } else if (условие) {
+//     действие
+// } 
+// else {
+//     действие
+// }
+
+
+let num1 = +prompt('Впишите число в диапазоне?')
+if (num1 >= 3  &&  num1 <= 5) {
+    console.log('Детский сад')
+}   else {
+    console.log('Ошибка')
+} 
+
+// Написать условие для возраста и места обучения
+// 3 - 5 - Детский сад
+// 6 - 17 - Школа
+// 18 - 25 - Универ
+// 25 - 30 - Снова универ
+// 30 + - Работяга
+// 50 + Внуки просто так
+
+//  || - или  && - и      != -не
+
+if (symbol == '+') {
+    num1 + num2
+}
